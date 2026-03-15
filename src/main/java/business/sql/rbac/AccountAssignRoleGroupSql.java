7dcac1a9b047184b34f197dcc2968d4f4385c1f4
Le Tan Vi <papylovevip1@gmail.com>
2026-03-15 07:23:04 +0700

    Leader Vi: Refactored StoresSql to clean Singleton and fixed merge mess

--- src/main/java/business/sql/rbac/AccountAssignRoleGroupSql.java
+++ src/main/java/business/sql/rbac/AccountAssignRoleGroupSql.java
@@ -12,6 +12,9 @@ public class AccountAssignRoleGroupSql {
 
     /**
      * Gán một tài khoản vào một nhóm quyền
+     * @param accountId
+     * @param roleGroupId
+     * @return 
      */
     public int assignAccountToGroup(String accountId, String roleGroupId) {
         // TODO: INSERT INTO ACCOUNT_ASSIGN_ROLE_GROUP (account_id, role_group_id) VALUES (?, ?)
@@ -20,6 +23,9 @@ public class AccountAssignRoleGroupSql {
 
     /**
      * Gỡ tài khoản khỏi nhóm quyền (Xóa vật lý hoặc xóa mềm tùy bạn chọn)
+     * @param accountId
+     * @param roleGroupId
+     * @return 
      */
     public int removeAccountFromGroup(String accountId, String roleGroupId) {
         // TODO: DELETE FROM ACCOUNT_ASSIGN_ROLE_GROUP WHERE account_id = ? AND role_group_id = ?
@@ -29,6 +35,8 @@ public class AccountAssignRoleGroupSql {
 
     /**
      * Lấy danh sách ID các nhóm quyền mà một tài khoản đang tham gia
+     * @param accountId
+     * @return 
      */
     public ArrayList<String> getGroupsByAccountId(String accountId) {
         // TODO: SELECT role_group_id FROM ACCOUNT_ASSIGN_ROLE_GROUP WHERE account_id = ? AND is_deleted = 0
@@ -37,6 +45,8 @@ public class AccountAssignRoleGroupSql {
 
     /**
      * Lấy danh sách ID các tài khoản thuộc một nhóm quyền nào đó
+     * @param roleGroupId
+     * @return 
      */
     public ArrayList<String> getAccountsByGroupId(String roleGroupId) {
         // TODO: SELECT account_id FROM ACCOUNT_ASSIGN_ROLE_GROUP WHERE role_group_id = ? AND is_deleted = 0
