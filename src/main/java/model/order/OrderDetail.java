f839ce1233e00cdc8c851c7a28691073890c35de
KhoiNguyen8326 <hkhoinguyen83@gmail.com>
2026-03-22 13:32:33 +0700

    Refactor Order & OrderDetail: add soft delete and fix constructors

--- src/main/java/model/order/OrderDetail.java
+++ src/main/java/model/order/OrderDetail.java
@@ -4,18 +4,21 @@ package model.order;
  * Model OrderDetail
  * Leader: Lê Tấn Vĩ (Đã dọn dẹp đống rác của Tùng)
  */
+    
 public class OrderDetail {
 
     private final String orderId;
     private final String productId;
     private final int quantity;
     private final double unitPrice;
+    private boolean isDeleted;
 
     public OrderDetail(String orderId, String productId, int quantity, double unitPrice) {
         this.orderId = orderId;
         this.productId = productId;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
+        this.isDeleted = false;
     }
 
     // Sửa lại để trả về giá trị thực tế thay vì ném lỗi
@@ -34,4 +37,14 @@ public class OrderDetail {
     public double getUnitPrice() {
         return unitPrice;
     }
+    
+    public boolean isDeleted() { 
+        return isDeleted; 
+    }
+    
+    //Setter
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+    
 }
