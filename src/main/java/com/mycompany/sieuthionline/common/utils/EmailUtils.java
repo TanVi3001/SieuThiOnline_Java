ad642a2980c7abe73c02e92929bf55c2e2f4dc62
DeeTung <24521949@gm.uit.edu.vn>
2026-04-10 09:29:45 +0700

    Xóa class ForgotPasswordView.java (do để nhầm định dạng)

--- src/main/java/com/mycompany/sieuthionline/common/utils/EmailUtils.java
+++ src/main/java/com/mycompany/sieuthionline/common/utils/EmailUtils.java
@@ -12,5 +12,32 @@ import javax.mail.internet.*;
  * @author nguye
  */
 public class EmailUtils {
-    
+    /**
+     * Hàm gửi mail linh hoạt với tài khoản người dùng tự nhập
+     */
+    public static void sendEmail(String fromEmail, String appPassword, String toEmail, String subject, String body) 
+            throws MessagingException {
+        
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        // Đăng nhập bằng tài khoản vừa nhập trên giao diện
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, appPassword);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(fromEmail));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+        message.setSubject(subject);
+        message.setText(body);
+
+        Transport.send(message);
+    }
 }
