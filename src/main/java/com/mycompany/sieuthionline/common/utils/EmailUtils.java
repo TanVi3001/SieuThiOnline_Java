0138e95def77dda59bda80cfdc58513e20f25165
DeeTung <24521949@gm.uit.edu.vn>
2026-04-10 09:23:38 +0700

    Thêm các class và các packages cho phần "Quên mật khẩu"

---
+++ src/main/java/com/mycompany/sieuthionline/common/utils/EmailUtils.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sieuthionline.common.utils;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+/**
+ *
+ * @author nguye
+ */
+public class EmailUtils {
+    
+}
